cd831a61ba20cbe2ad1e6c0bdb49f2b73ef413a3
davidrusycki <davidrusyck@gmail.com>
2022-10-14 20:00:24 -0300

    first test

--- front-end/src/chat.js
+++ front-end/src/chat.js
@@ -1,3 +1,18 @@
-let websocket = new Websocket();
+let websocket = new Websocket("wss://localhost:9191");
 
+websocket.onopen = (message) => {
+    console.log('Conexão aberta.');
+    websocket.send("Meu nome é david");
+}
 
+websocket.onmessage = (message) => {
+    console.log(`[Mensagem] ${message.mensagem}`);
+}
+
+websocket.onclose = (message) => {
+    console.log("Conexão fechada");
+}
+
+websocket.onerror = (message) => {
+
+}
